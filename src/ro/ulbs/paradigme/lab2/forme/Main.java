3034f280a5c31e7646bc650126d8d2ed51144a35
torchu1o <tudorsebastian.torcica@ulbsibiu.ro>
2025-03-14 10:03:57 +0200

    LAB 2

---
+++ src/ro/ulbs/paradigme/lab2/forme/Main.java
@@ -0,0 +1,16 @@
+package ro.ulbs.paradigme.lab2.forme;
+class Main{
+
+    public static void main(String[] args) {
+        Triangle tri = new Triangle(1.1F, 2.0F, "red");
+        Circle c = new Circle(1.5F, "yellow");
+        Square sq = new Square(1.2F, "blue");
+        System.out.println("Area = " + tri.getArea()+" details: " + tri);
+        System.out.println("Area = " + c.getArea() + " details: " + c);
+        System.out.println("Area = " + sq.getArea() + " details: " + sq);
+        Triangle tri2 = new Triangle(1.1F, 2.0F, "red");
+        System.out.println("Triangle2 equals to Triangle1: " + tri.equals(tri2));
+        Triangle tri3 = new Triangle(1.1F, 2.0F, "brown");
+        System.out.println("Triangle3 equals to Triangle1: " + tri.equals(tri3));
+    }
+}
